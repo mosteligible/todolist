34386b9e9e877a4e92e11f4f34b77b428c01c31d
mosteligible <mosteligible@mosteligible>
2022-06-05 14:57:20 -0400

    tests to work with formatted todo list

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -64,7 +64,7 @@ func TestTodoCLI(t *testing.T) {
 			t.Fatalf("%s", err)
 		}
 
-		expected := task + "\n"
+		expected := "   1: " + task + "\n"
 
 		if expected != string(out) {
 			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
