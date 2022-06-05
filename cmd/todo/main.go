ea7aaee40fda2aa06078cba5e6c8fc6716497823
mosteligible <mosteligible@mosteligible>
2022-06-04 23:13:08 -0400

    Add cli parser with  package

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/mosteligible/todolist"
 )
@@ -11,6 +11,15 @@ import (
 const todoFilename = ".todo.json"
 
 func main() {
+	// parse cli commands
+	task := flag.String("task", "", )
+
+	task := flag.String("task", "", "Task to be included in the ToDo list.")
+	list := flag.Bool("list", false, "List the items in ToDo list.")
+	complete := flag.Int("complete", , "Index of item to be marked complete.")
+
+	flag.Parse()
+
 	l := &todolist.List{}
 
 	if err := l.Get(todoFilename); err != nil {
@@ -19,16 +28,30 @@ func main() {
 	}
 
 	switch {
-	case len(os.Args) == 1:
+	case *list:
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
 		}
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
-		if err := l.Save(todoFilename); err != nil {
+	case *complete:
+		if err := l.Complete(*complete); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+		if err := l.Save(todoFilename); err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *task:
+		l.Add(*task)
+
+		if err := l.Save(todoFilename); err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid flag provided!")
+		os.Exit(1)
 	}
 }
